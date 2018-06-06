6eb3586bdd0dffd07fbdcfb4c9248c6d8f95aa70
lf <18073072@qq.com>
2018-06-06 17:06:03 +0800

    2018.5.21

--- .idea/self_code/markovModels.py
+++ .idea/self_code/markovModels.py
@@ -26,57 +26,72 @@ import tensorflow as tf
      #       策略2：状态2
      #
 
-#输入每个状态的决策集，构造决策序列
+#输入每个状态的决策集，构造决策序列,只能构造每个状态策略都相同的
 def active_road(active_vector=tf.zeros([4,4])):
-    g=tf.Graph()
-    with g.as_default():
-         a=tf.constant(1)
-         result=tf.zeros(0)
-         len=active_vector.shape[0]
+     tf.get_default_graph()
+     a=tf.constant(1)
+     result=tf.zeros(5)
+     len=active_vector.shape[0]
+     len_colume=active_vector.shape[1]
+     with tf.Session() as sess:
          for i in range(len-1):
              if i==0:
                 s_a_matrix_x,s_a_matrix_y=tf.meshgrid(active_vector[i],active_vector[i+1])
                 result=tf.concat([tf.reshape(s_a_matrix_x,[-1,1]),tf.reshape(s_a_matrix_y,[-1,1])],1)
              else:
-                temp_vector=tf.zeros(result.shape[0])
+                result_temp=tf.tile(result,[len_colume,1])
+                temp_vector=tf.zeros(result.shape[0],dtype=tf.int32)
                 s_a_matrix_x,s_a_matrix_y=tf.meshgrid(temp_vector,active_vector[i+1])
-                result=tf.concat([tf.reshape(s_a_matrix_x,[-1,1]),tf.reshape(s_a_matrix_y,[-1,1])],1)
+                # print(sess.run(s_a_matrix_y))
+                result=tf.concat([result_temp,tf.reshape(s_a_matrix_y,[-1,1])],1)
+                # print(sess.run(result))
+         result=sess.run(result)
+     return np.array(result)
 
-    sess=tf.Session(graph=g)
-    print(sess.run(a))
-    sess.close()
-    return 1
+
+#返回不同策略组合下的转移概率矩阵[1,1,0.5,0.5]状态1在行动1下，跳转到1和2状态的概率分别是0.5和0.5
+def P_fun(a=tf.zeros(10),s=tf.zeros(10),f=np.zeros((10,10)),p=np.array([[1,1,0.5,0.5],[1,2,0.8,0.2],[2,1,0.4,0.6],[2,2,0.7,0.3]])):
+    tf.get_default_graph()
+    result=[]
+    s_len=s.shape[0]#s.shape[0]==a.shape[0],状态个数
+    f_len=f.shape[0]#策略路径的个数
+    with tf.Session() as sess:
+        # p=sess.run(p)
+        for i in range(f_len):#每个策略路径构成一个转移矩阵
+              temp_matrix=np.zeros((s_len,s_len))
+              for j in range(f[i].shape[0]): #转移矩阵的行数
+                    for x in p:
+                        if x[0]==j+1 and x[1]==f[i][j]:
+                           temp_matrix[j]=x[2:np.array(x).__len__()+1]
+                        #矩阵行数对应       #对应策略
+              result.append(temp_matrix)
+    return f,result
 
 #（状态，收益）矩阵
 #状态向量
-s=tf.constant([3,4]);
-
-#策略向量
-a=tf.constant([1,2]);
+s=tf.constant([1,2]);
 
 #决策函数序列f1,f2,f3,f4
-s_a_matrix_x,s_a_matrix_y=tf.meshgrid(s,a)
-f=tf.concat([tf.reshape(s_a_matrix_x,[4,1]),tf.reshape(s_a_matrix_y,[4,1])],1)
-# [[1 1]
-#  [2 1]
-# [1 2]
-# [2 2]]
+a=tf.constant([[1,2],[1,2]])
+f=active_road(a)
 
 #依据f生成不同的概率转移矩阵
 #转移到            状态1 状态2       状态1 状态2         状态1 状态2          状态1 状态2
 #不同状态下选者的策略   [1 1]            [2 1]             [1 2]               [2 2]
 #状态转移矩阵 p(1|1,1),p(1|2,1) p(1|1,2),p(2|2,1)   p(1|1,1),p(2|1,2)   p(1|1,2),p(2|2,2)
-p=tf.constant([[0.5,0.5],[0.4,0.6],[0.8,0.2],[0.7,0.3]])
 
-#收益报酬
-r=tf.constant([6,4,-3,-5])
+p=tf.constant([[1,1,0.5,0.5],[1,2,0.4,0.6],[2,1,0.8,0.2],[2,2,0.7,0.3]])
+
+
+#收益报酬[1,1,6]表示i=1,a=1,状态在1,策略为1下的收益为6
+r=tf.constant([[1,1,6],[1,2,4],[2,1,-3],[2,2,-5]])
 
 #总报酬函数、最优值函数v（i，pi）
 
 #第一次迭代，取方案
 #迭代求解方程最优解法
+f,p_matrix=P_fun(a,s,f)
+print(p_matrix)
 with tf.Session() as sess:
+     f_len=f.shape[0]
 
-     print(np.array(sess.run(tf.meshgrid(s,a))))
-
-active_road(tf.constant([[1,2],[3,4]]))
