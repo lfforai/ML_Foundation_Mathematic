136fa72d75b130a208b9a4a5d71d93e0791abf56
lf <18073072@qq.com>
2018-06-11 17:10:21 +0800

    6.1

--- .idea/self_code/markovModels.py
+++ .idea/self_code/markovModels.py
@@ -68,14 +68,127 @@ def P_fun(a=tf.zeros(10),s=tf.zeros(10),f=np.zeros((10,10)),p=np.array([[1,1,0.5
     return f,result
 
 #迭代计算最优结果,f是策略函数集合，p_matrix_list转移矩阵list,v报酬函数
-def max_find(f,p_matrix_list,v):
-    s_num=f[0].shape[0]
-    s_temp=np.zeros(s_num)
+def max_find_3_1(a,f,p_matrix_list,r):
+    tf.get_default_graph()
+    s_num=f[0].shape[0]#状态数
+    f_num=f.shape[0]
+    s_temp=np.zeros((s_num,2))#每个状态会选择一个action
     for i in range(s_num):
-        s_temp
-    v=tf.reshape(tf.slice(tf.gather(tf.cast(r,dtype=tf.float32),[0,2]),[0,2],[2,1]),[-1])
+        s_temp[i]=[i+1,f[0][i]]
 
-    print(s_num)
+    #提取v的数值
+    list_temp=[]
+    v_temp=tf.Variable(tf.zeros([s_num]))
+    init_op = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        print("r:=",sess.run(r))
+        print("f=",f,f_num)
+        print("s_temp:=",s_temp)
+        print("p_matrix_list:=",p_matrix_list)
+        r_h=sess.run(r)
+        a_h=sess.run(a)
+
+        for i in range(r_h.shape[0]):
+            for j in range(s_temp.shape[0]):
+                # print(r_h[i][0],s_temp[j][0],r_h[i][1],s_temp[j][1])
+                if r_h[i][0]==s_temp[j][0] and r_h[i][1]==s_temp[j][1]:
+                   # print("ok:=",r_h[i][0],s_temp[j][0],r_h[i][1],s_temp[j][1])
+                   list_temp.append(i)
+
+        # print("list_temp:=",list_temp)
+        #初始化第一个状态
+        r_m=tf.reshape(tf.slice(tf.gather(tf.cast(r,dtype=tf.float32),list_temp),[0,2],[2,1]),[-1])
+        p=tf.convert_to_tensor(tf.cast(p_matrix_list[0],dtype=tf.float32))
+        I=tf.eye(s_num)
+        v=tf.reduce_sum(tf.multiply(tf.matrix_inverse(I-0.9*p),r_m),axis=1)
+        v_1=tf.reduce_max(v,axis=0)
+
+        print("v_1:=",sess.run(v_1))
+        print("------------开始迭代-------------------")
+
+        max_f_index_1=0#n次迭代中最大函数f的index
+
+        #查找在f[i]满足，条件f[i][index]=a,其余f[i][j]=f_value=[j],j!=index,返回i
+        def e_A_B_list(index,a,f_value=[],f=[]):
+            result=-1
+            for i in range(f.__len__()):
+                true=0
+                for j in range(list(f[0]).__len__()):
+                    if (f[i][j]==f_value[j] and j!=index) or (j==index and f[i][index]==a):
+                       true=true+1
+                if true==list(f[0]).__len__():
+                   result=i
+                   break
+        return result
+
+        #循环迭代求最优的v值
+        while(True):
+            #计算使得v最大的f
+            temp_value=0
+            max_f_index_2=0
+            list=[]
+            for index_f in range(s_num): #每个状态的
+               for e in range(list(a_h[index_f]).__len__()): #每个策略都进行一次遍历，找到最优值
+                 #找到对应的f中的元素f[max]
+                 max_f=e_A_B_list(e,a_h[index_f][e],f)
+                 list_temp=[]
+                 for i in range(s_num):
+                     s_temp[i]=[i+1,f[max_f][i]]
+                 # print("s_temp:=",s_temp)
+                 for i in range(r_h.shape[0]):
+                     for j in range(s_temp.shape[0]):
+                         # print(r_h[i][0],s_temp[j][0],r_h[i][1],s_temp[j][1])
+                         if r_h[i][0]==s_temp[j][0] and r_h[i][1]==s_temp[j][1]:
+                            # print("ok:=",r_h[i][0],s_temp[j][0],r_h[i][1],s_temp[j][1])
+                            list_temp.append(i)
+
+                 # print("list_temp:=",list_temp)
+                 r_m=tf.reshape(tf.slice(tf.gather(tf.cast(r,dtype=tf.float32),list_temp),[0,2],[2,1]),[-1])
+                 p=tf.convert_to_tensor(tf.cast(p_matrix_list[max_f],dtype=tf.float32))
+                 v_temp=tf.add(r_m,tf.reduce_sum(tf.multiply(0.9*p,v),axis=1))
+
+                 v_2=tf.reduce_max(v_temp,axis=0)
+                 print("v_temp：=",sess.run(v_temp))
+
+                 if max_f==0:
+                    temp_value=sess.run(v_2)
+
+                 temp_media=sess.run(v_2)
+                 if temp_media>temp_value and max_f!=0:
+                    max_f_index_2=max_f
+                    temp_value=temp_media
+
+               print("n+1次最好的策略是：=",f[max_f_index_2],"n最好的策略是：=",f[max_f_index_1])
+               print("n+1次最好值：=",sess.run(v_temp))
+               if max_f_index_1==max_f_index_2:
+                   print("break out!")
+                   break
+               else:
+                   max_f_index_1=max_f_index_2
+
+            #回到步骤2
+            list_temp=[]
+            for i in range(s_num):
+                s_temp[i]=[i+1,f[max_f_index_1][i]]
+
+            for i in range(r_h.shape[0]):
+                for j in range(s_temp.shape[0]):
+                    # print(r_h[i][0],s_temp[j][0],r_h[i][1],s_temp[j][1])
+                    if r_h[i][0]==s_temp[j][0] and r_h[i][1]==s_temp[j][1]:
+                        # print("ok:=",r_h[i][0],s_temp[j][0],r_h[i][1],s_temp[j][1])
+                        list_temp.append(i)
+
+            #初始化第一个状态
+            r_m=tf.reshape(tf.slice(tf.gather(tf.cast(r,dtype=tf.float32),list_temp),[0,2],[2,1]),[-1])
+            p=tf.convert_to_tensor(tf.cast(p_matrix_list[max_f_index_1],dtype=tf.float32))
+            I=tf.eye(s_num)
+            v=tf.reduce_sum(tf.multiply(tf.matrix_inverse(I-0.9*p),r_m),axis=1)
+            # v_1=tf.reduce_max(v,axis=0)
+
+        print("max f :=",max_f_index_2)
+        print(sess.run(v_temp))
+    return 1
 
 #（状态，收益）矩阵
 #状态向量
@@ -100,23 +213,8 @@ r=tf.constant([[1,1,6],[1,2,4],[2,1,-3],[2,2,-5]])
 #第一次迭代，取方案
 #迭代求解方程最优解法
 f,p_matrix_list=P_fun(a,s,f)
-max_find(f,p_matrix_list,r)
+max_find_3_1(f,p_matrix_list,r)
 
 
 with tf.Session() as sess:
      pass
-
-      # print("---------------------------------")
-      # v=tf.reshape(tf.slice(tf.gather(tf.cast(r,dtype=tf.float32),[0,2]),[0,2],[2,1]),[-1])
-      # p=tf.convert_to_tensor(tf.cast(p_matrix_list[0],dtype=tf.float32))
-      # I=tf.eye(2)
-      # result=tf.reduce_sum(tf.multiply(tf.matrix_inverse(I-0.9*p),v),axis=1)
-      # print(sess.run(result))
-
-
-      #print(sess.run(tf.reduce_sum(tf.multiply(tf.matrix_inverse(tf.subtract(tf.eye(2,dtype=tf.float32),0.9*tf.convert_to_tensor(tf.cast(p_matrix_list[1],dtype=tf.float32)))),,axis=1)))
-
-
-
-      # c = tf.matmul(a, b)
-      # print(sess.run(c))
